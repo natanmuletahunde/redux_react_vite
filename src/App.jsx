3433e8b9c1f38cf8c91aa40d02953ec7664adf7c
pivnati <natanmuleta911@gmail.com>
2024-08-20 18:31:29 -0700

    use the bootstrap for the card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Product from './components/Product'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
  
 
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,6 @@
 import { useState ,useEffect } from "react";
+ import Card from 'react-bootstrap/Card';
+ 
 const Product = ()=>{
 const [products, getProduct] = useState([]);
   useEffect(()=>{
@@ -7,11 +9,24 @@ const [products, getProduct] = useState([]);
     .then(data=>data.json() )
     .then(Result=>getProduct(Result))
   },[]);
-  
+  const cards = products.map(product=>(
+   <div className="col-md-3">
+   <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> 
+   </div>      
+))
   return (
         <div>
             <h1>Product DashBoard</h1>
-            {JSON.stringify(products) }
+            <div className="row">
+                {cards}
+            </div>
         </div>
     ) 
 }
